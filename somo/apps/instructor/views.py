d25b469a29eb44923bf5bf6b71b7e0d541a4ed37
dennisnderitu254 <dknderitu@gmail.com>
2019-07-30 15:55:40 +0300

    This is the somo app

---
+++ somo/apps/instructor/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views.generic import View, TemplateView
+
+
+class Index(View):
+    template_name = 'index.html'
+
+class About(View):
+    template_name = 'about.html'
+
+class Contacts(View):
+    template_name = 'contacts.html'
